6bb4d1a46c8f94cc66902f2a012252276ece3d34
Parth Bhatia <parthbhatia98@gmail.com>
2019-02-16 21:19:57 +0530

    X00T: inherit perf blobs from vendor/qcom/common
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Signed-off-by: Mihran Thalhath <mihranz7@gmail.com>

--- device.mk
+++ device.mk
@@ -318,6 +318,9 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Performance
+-include vendor/qcom/common/qti-vendor.mk
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
